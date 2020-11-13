4bd0727d4bcc3d12d2877cd0087eb024f194a826
biancahpp <bianca_procopio@hotmail.com>
2020-11-12 18:03:13 -0600

    damn arrow

--- script.js
+++ script.js
@@ -2,9 +2,26 @@ const shareBtn = document.getElementById('share-btn');
 
 function share () {
 
-    const avatar = document.querySelector('.avatar-wrapper');
+    const avatar = document.querySelector('.coco');
+    const icon = document.querySelector('.share-icon');
+    const backg = document.querySelector('.michelle-img');
+    const buttonIcon = document.querySelector('#share-icon');    
 
-    avatar.style.display == 'none' ? avatar.style.display = 'block' : avatar.style.display = 'none';
+    if (avatar.style.display !== 'none') {
+        avatar.style.display = 'none';
+        backg.style.background = 'hsl(217, 19%, 35%)';
+        icon.style.display = 'flex';
+        shareBtn.style.background = 'hsl(214, 17%, 51%)';
+        buttonIcon.style.fill = 'red';
+        
+    }
+    else if (avatar.style.display == 'none'){
+        avatar.style.display = 'flex';
+        backg.style.background = '#fff';
+        icon.style.display = 'none';
+        shareBtn.style.background = 'hsl(210, 46%, 95%)';
+    }
+
 }
 
-shareBtn.addEventListener('click', share)
+shareBtn.addEventListener('click', share);
